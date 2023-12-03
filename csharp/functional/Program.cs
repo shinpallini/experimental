b7f2848b80f3639739c83bcfd7244ff1ad1dbe1f
shin <49378881+shinpallini@users.noreply.github.com>
2023-12-03 05:57:08 +0000

    functionalパターンでのサンプルを作成

--- csharp/functional/Program.cs
+++ csharp/functional/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+public class HttpRequestExample
+{
+    public static async Task Main()
+    {
+        var httpRequestExample = new HttpRequestExample();
+        var result = await httpRequestExample.FetchDataAsync("http://example.com");
+
+        result.Match(
+            Right: responseBody => Console.WriteLine("Success: " + responseBody),
+            Left: errorMessage => Console.WriteLine("Error: " + errorMessage)
+        );
+
+    }
+    public async Task<Either<string, string>> FetchDataAsync(string url)
+    {
+        using (var client = new HttpClient())
+        {
+            try
+            {
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return Right<string, string>(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                return Left<string, string>(e.Message);
+            }
+        }
+    }
+}
+
