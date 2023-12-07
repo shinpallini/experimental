a60c590ae0354b2c903a531253dcf1df42537fab
pallini <shinpall1ni@gmail.com>
2023-12-08 00:24:59 +0900

    add: mondai18

--- haskell/algorithm/mondai18.hs
+++ haskell/algorithm/mondai18.hs
@@ -1,11 +1,18 @@
-import qualified Data.Set as Set
+import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
 
-countPairs :: [Int] -> Int
-countPairs prices = length [(x, y) | x <- prices, y <- prices, x + y == 500] `div` 2
+countOccurrences :: (Ord k, Num a) => [k] -> Map.Map k a
+countOccurrences xs = Map.fromListWith (+) [(x, 1) | x <- xs]
 
+lookup' :: (Ord k, Num a) => Map.Map k a -> k -> a
+lookup' prices key = Maybe.fromMaybe 0 (Map.lookup key prices)
+
+main :: IO ()
 main = do
     _ <- getLine
     input <- getLine
     let prices = map read $ words input :: [Int]
-    print $ countPairs prices
-
+    -- let priceLookup = lookup' (countOccurrences prices)
+    -- print $ priceLookup 100 * priceLookup 400 + priceLookup 200 * priceLookup 300
+    let pricesMap = countOccurrences prices
+    print $ pricesMap Map.! 100 * pricesMap Map.! 400 + pricesMap Map.! 200 * pricesMap Map.! 300
