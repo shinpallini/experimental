ae9916c6a6a7d2278b1d108c902687b517ff2fa0
pallini <shinpall1ni@gmail.com>
2023-12-09 21:26:48 +0900

    add type switch example

--- golang/list/main.go
+++ golang/list/main.go
@@ -13,8 +13,18 @@ func Map[T any, U any](f func(T) U, l []T) []U {
 	return newList
 }
 
+func doSomething[bytes []byte | string](s bytes) {
+	fmt.Println(s)
+}
+
 func main() {
 	l := []int{1, 2, 3, 4, 5}
 	newList := Map(func(x int) string { return "value:" + strconv.Itoa(x+1) }, l)
 	fmt.Println(newList)
+	switch v := interface{}(newList).(type) {
+	case []int:
+		fmt.Println("int slice")
+	default:
+		fmt.Printf("Value: %v, Type: %T\n", v, v)
+	}
 }
