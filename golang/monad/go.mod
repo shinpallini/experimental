f5768d57f1a2a87fc1b13380ead675566d959b85
pallini <shinpall1ni@gmail.com>
2023-12-09 19:57:33 +0900

    create IO Monad using golang

---
+++ golang/monad/go.mod
@@ -0,0 +1,3 @@
+module monad
+
+go 1.20
---
+++ golang/monad/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// IOMonadは、入出力操作をカプセル化するための関数型です。
+type IOMonad func() string
+
+// doInputは、標準入力から文字列を読み込むIOMonadを生成します。
+func doInput() IOMonad {
+	return func() string {
+		sc := bufio.NewScanner(os.Stdin)
+		sc.Scan()
+		return sc.Text()
+	}
+}
+
+// doPrintは、与えられた文字列を標準出力に出力するIOMonadを生成します。
+func doPrint(s string) IOMonad {
+	return func() string {
+		fmt.Println(s)
+		return ""
+	}
+}
+
+// Bindは、IOMonadを実行し、その結果を別のIOMonadにバインド（適用）する関数です。
+func Bind(m IOMonad, f func(string) IOMonad) IOMonad {
+	input := m()
+	result := f(input)
+	return result
+}
+
+func main() {
+	// Bindで連結することで、時間依存のない標準入力と標準出力を実現できます。
+	action := Bind(doInput(), doPrint)
+	action()
+}
