a9d0ef7e4bdad3f4de40617185f90a7bf6d0c71e
pallini <shinpall1ni@gmail.com>
2023-12-17 22:42:30 +0900

    add lesson19

---
+++ haskell/book/lesson19.hs
@@ -0,0 +1,25 @@
+import Data.Map qualified as Map
+import Data.Maybe
+
+data Organ = Heart | Brain | Kidney | Spleen deriving (Show, Eq)
+
+organs :: [Organ]
+organs = [Heart, Heart, Brain, Spleen, Spleen, Kidney]
+
+ids :: [Int]
+ids = [2, 7, 13, 14, 21, 24]
+
+organPairs :: [(Int, Organ)]
+organPairs = zip ids organs
+
+organCatalog :: Map.Map Int Organ
+organCatalog = Map.fromList organPairs
+
+possibleDrawers :: [Int]
+possibleDrawers = [1 .. 50]
+
+getDrawerContents :: [Int] -> Map.Map Int Organ -> [Maybe Organ]
+getDrawerContents ids catalog = map (\x -> Map.lookup x catalog) ids
+
+availableOrgans :: [Maybe Organ]
+availableOrgans = getDrawerContents possibleDrawers organCatalog
