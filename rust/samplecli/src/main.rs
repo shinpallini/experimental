c0622dd98c959a25cb470b3cca2cd1515aabd614
shin <49378881+shinpallini@users.noreply.github.com>
2023-12-29 17:24:24 +0000

    add test and sample code

--- rust/samplecli/src/main.rs
+++ rust/samplecli/src/main.rs
@@ -94,3 +94,29 @@ fn main() {
     println!("Is verbosity specified?: {}", opts.verbose);
     println!("Is num specified?: {}", opts.num.unwrap_or_default());
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ok() {
+        let calc = RpnCalculator::new(false);
+        assert_eq!(calc.eval("5"), 5);
+        assert_eq!(calc.eval("50"), 50);
+        assert_eq!(calc.eval("-50"), -50);
+
+        assert_eq!(calc.eval("2 3 +"), 5);
+        assert_eq!(calc.eval("2 3 *"), 6);
+        assert_eq!(calc.eval("2 3 -"), -1);
+        assert_eq!(calc.eval("2 3 /"), 0);
+        assert_eq!(calc.eval("2 3 %"), 2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_ng() {
+        let calc = RpnCalculator::new(false);
+        calc.eval("1 1 ~");
+    }
+}
