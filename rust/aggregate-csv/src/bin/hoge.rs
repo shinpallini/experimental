9d1606e296d34166cc1320d9bd6830baa9d50691
shin <49378881+shinpallini@users.noreply.github.com>
2024-02-08 16:27:16 +0000

    :hammer: add sample

--- rust/aggregate-csv/src/bin/hoge.rs
+++ rust/aggregate-csv/src/bin/hoge.rs
@@ -1,9 +1,9 @@
-use std::fs::File;
-use std::env;
-use std::io::{self, BufReader};
+use csv::ReaderBuilder;
 use encoding_rs::*;
 use encoding_rs_io::DecodeReaderBytesBuilder;
-use csv::ReaderBuilder;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufReader};
 
 fn main() -> io::Result<()> {
     // CSVファイルを開く
@@ -19,8 +19,7 @@ fn main() -> io::Result<()> {
     );
 
     // CSVリーダーを作成し、UTF-8として処理する
-    let mut rdr = ReaderBuilder::new()
-        .from_reader(transcoded_reader);
+    let mut rdr = ReaderBuilder::new().from_reader(transcoded_reader);
 
     for result in rdr.records() {
         let record = result?;
